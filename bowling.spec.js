07109909351a6dbbfa051fde56db11743bcadebf
Valentin Fourny <valentin.fournier@anatec.io>
2023-04-06 13:35:02 +0200

    deocmment test
    Signed-off-by: Valentin Fourny <valentin.fournier@anatec.io>

--- bowling.spec.js
+++ bowling.spec.js
@@ -1,59 +1,59 @@
 import { Game, Frame } from './bowling'
 
-// describe('Game', () => {
-//     describe('a game with no rolls', () => {
-//         const game = new Game()
-//         it('scores 0 points', () => {
-//             expect(game.score).toEqual(0)
-//         })
-//     })
+describe('Game', () => {
+    describe('a game with no rolls', () => {
+        const game = new Game()
+        it('scores 0 points', () => {
+            expect(game.score).toEqual(0)
+        })
+    })
 
-//     describe('a game with no pins knocked down', () => {
-//         const game = new Game()
-//         const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-//         rolls.forEach((roll) => {
-//             game.roll(roll)
-//         })
-//         it('scores 0 points', () => {
-//             expect(game.score).toEqual(0)
-//         })
-//     })
+    describe('a game with no pins knocked down', () => {
+        const game = new Game()
+        const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        rolls.forEach((roll) => {
+            game.roll(roll)
+        })
+        it('scores 0 points', () => {
+            expect(game.score).toEqual(0)
+        })
+    })
 
-//     describe('a game starting with 3 strikes', () => {
-//         const game = new Game()
-//         const rolls = [10, 10, 10]
-//         rolls.forEach((roll) => {
-//             game.roll(roll)
-//         })
-//         it('scores 60 points', () => {
-//             expect(game.score).toEqual(60)
-//         })
-//     })
+    describe('a game starting with 3 strikes', () => {
+        const game = new Game()
+        const rolls = [10, 10, 10]
+        rolls.forEach((roll) => {
+            game.roll(roll)
+        })
+        it('scores 60 points', () => {
+            expect(game.score).toEqual(60)
+        })
+    })
 
-//     describe('a game starting with a mix of strikes, spares, and open frames', () => {
-//         const game = new Game()
-//         const rolls = [10, 2, 7, 5, 5, 6, 3]
-//         rolls.forEach((roll) => {
-//             game.roll(roll)
-//         })
-//         it('be scored appropriately', () => {
-//             expect(game.score).toEqual(
-//                 10 + 2 + 7 + (2 + 7) + (10 + 6) + (6 + 3)
-//             )
-//         })
-//     })
+    describe('a game starting with a mix of strikes, spares, and open frames', () => {
+        const game = new Game()
+        const rolls = [10, 2, 7, 5, 5, 6, 3]
+        rolls.forEach((roll) => {
+            game.roll(roll)
+        })
+        it('be scored appropriately', () => {
+            expect(game.score).toEqual(
+                10 + 2 + 7 + (2 + 7) + (10 + 6) + (6 + 3)
+            )
+        })
+    })
 
-//     describe('a perfect game', () => {
-//         const game = new Game()
-//         const rolls = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
-//         rolls.forEach((roll) => {
-//             game.roll(roll)
-//         })
-//         it('scores 300 points', () => {
-//             expect(game.score).toEqual(300)
-//         })
-//     })
-// })
+    describe('a perfect game', () => {
+        const game = new Game()
+        const rolls = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+        rolls.forEach((roll) => {
+            game.roll(roll)
+        })
+        it('scores 300 points', () => {
+            expect(game.score).toEqual(300)
+        })
+    })
+})
 
 describe('Frame', () => {
     describe('an open frame', () => {
